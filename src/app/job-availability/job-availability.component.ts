b073ac6c82fa70381f81afdd87f22a95ca478146
iamwatchdogs <shamith3011022@gmail.com>
2024-05-02 22:40:39 +0530

    Created job-availability component

---
+++ src/app/job-availability/job-availability.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-availability',
+  templateUrl: './job-availability.component.html',
+  styleUrl: './job-availability.component.css'
+})
+export class JobAvailabilityComponent {
+
+}
